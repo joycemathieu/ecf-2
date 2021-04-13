85557214e11d0fc6c928192b0700a4ea22720239
joycemathieu <j.matthieu@cci-formation.nc>
2021-04-13 12:01:11 +1100

    ajout js

---
+++ app.js
@@ -0,0 +1,6 @@
+function onClickOn(){
+    const popUp = document.getElementById('pop-up-container');
+    popUp.style.opacity = '1';
+};
+
+document.getElementById('cart').addEventListener('click',onClickOn);
